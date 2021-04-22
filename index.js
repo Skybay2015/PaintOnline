b516246d810c7a944c0bec4b92237d66a38dd938
Fedir Tsarenko <44132297+Skybay2015@users.noreply.github.com>
2021-04-22 17:17:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,20 +12,8 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-const whitelist = ['http://localhost:3000'​, 'http://localhost:8080'​, 'https://desolate-mesa-61830.herokuapp.com'​]
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
+
+app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
    // Serve any static files
